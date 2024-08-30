f666af33e39328a7b0078ce61ff26a0d88b68747
Solis-Chavez <rsolis12@cps.k12.il.us>
2024-08-30 14:55:00 -0500

    first commit

---
+++ lesson2.py
@@ -0,0 +1,61 @@
+# create a print fuction that prints out print Hello, World!
+# print("Hello, World!")
+# ask the user for the day of the week 
+# day_of_the_week = input("What day of the week is it? ")
+# print("Today is " + day_of_the_week)
+# concatenation is when you add two strings togteher  
+# using a plus sign (+)
+# movies_this_week = input("What movies are you watching this week? ")
+# print("I am watching " + movies_this_week + " this week. ")
+# mood = input("How are you feeling today? ")
+# print("I am feeling " + mood )
+
+# data types fpr variables om python
+# strings - text
+name = "John" #this is a string data type
+# whenevr it is wrapped around quoetes it is a string
+year = "2024" 
+# intergers - whole numbers
+year = 2024 #this is a interger data type
+# does not need to be wrapped in quotes
+yearFourFromNow = 2028
+subtract = yearFourFromNow - year
+print(subtract)
+# floats - demical numbers
+priceBigMac = 3.99 # This is a float data type
+priceDoublePounder = 4.99
+totalPrice = priceBigMac + priceDoublePounder
+print(totalPrice)
+# needs to have a decimal point
+# bootleans - True or False
+isRaining = False # this is a boolean data type
+print(isRaining)
+# list - a collection of items
+groceries = ["apples", "bananas", "carrots"]
+print(groceries)
+# challenge #1
+# you and your family are going to a movie and dinner. You need a list of movies
+# to choose from the
+# the place of a restuarent
+# address of resturant
+# the time of the movie
+# the time of the dinner
+# the name of the movie
+# the price of dinner
+# check to see if the movie is playing in the evening
+# how many people are going
+# print the entire output in one sentence 
+listofmovies = ["Dead pool", "The Crow", "BeatleJuice", "The Lion King"]
+restuarentAdress= "1234 Main St"
+dinnerTime = "7:00pm"
+movieTime = "5:00PM"
+movieName = "The Crow"
+moveiPrice = 12.99
+dinnerprice= 15.99
+inevening = True
+peoplegoing= input("How many people are going?")
+# type converserionn - converting from one data type to another 
+# print the entire output in one senetence
+print("We are going to see" + movieName +" at " + movieTime +" and then eat dinner at " + restuarentAdress + "at" + dinnerTime + "with " +peoplegoing+"people")
+
+
